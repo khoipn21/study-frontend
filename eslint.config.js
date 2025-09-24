7e0832fe075c7ecd8769c6d5d6b452dd9fcfccd3
khoipn21 <khoingoc456@gmail.com>
2025-09-24 13:57:13 +0700

    chore: disable unused variable rule in ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -58,6 +58,7 @@ export default [
       '@typescript-eslint/prefer-optional-chain': 'warn',
       '@typescript-eslint/no-non-null-assertion': 'warn',
       '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': 'off',
     },
     settings: {
       'import-x/resolver': {
